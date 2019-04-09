3509cb6399dcb885b6ae16664c1cf412a5efa804
Loshan T <loshan1212@gmail.com>
2019-04-09 08:04:41 +0100

    debug: react-native-debugger support

--- store.js
+++ store.js
@@ -1,11 +1,14 @@
-import { createStore, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
+import { createStore, applyMiddleware, compose } from 'redux';
 import { persistStore, persistReducer } from 'redux-persist';
 import createSensitiveStorage from 'redux-persist-sensitive-storage';
+import thunk from 'redux-thunk';
 import reducer from './src/reducers';
 
 const initialState = {};
 const middleware = [thunk];
+
+// eslint-disable-next-line no-underscore-dangle
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const storage = createSensitiveStorage({
   keychainService: 'lndmobileKeychain',
@@ -18,5 +21,9 @@ const persistConfig = {
 };
 const pReducer = persistReducer(persistConfig, reducer);
 
-export const store = createStore(pReducer, initialState, applyMiddleware(...middleware));
+export const store = createStore(
+  pReducer,
+  initialState,
+  composeEnhancers(applyMiddleware(...middleware))
+);
 export const pStore = persistStore(store);
