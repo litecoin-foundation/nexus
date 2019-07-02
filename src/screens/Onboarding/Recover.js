43889dcbed14efead7475d478e13f2a084e0a4a5
losh11 <loshan1212@gmail.com>
2019-07-02 22:38:37 +0100

    refactor(onboarding): wallet recovery

--- src/screens/Onboarding/Recover.js
+++ src/screens/Onboarding/Recover.js
@@ -1,48 +1,99 @@
-import React, { Component } from 'react';
-import { View, Text, TextInput } from 'react-native';
-import { connect } from 'react-redux';
+import React, { createRef, useRef, useEffect, useState } from 'react';
+import { View, Text, TextInput, FlatList, SafeAreaView, KeyboardAvoidingView } from 'react-native';
+import { useNavigation } from 'react-navigation-hooks';
+import { useDispatch } from 'react-redux';
+import LinearGradient from 'react-native-linear-gradient';
 
-import Form from '../../components/Onboarding/Form';
 import { recoverSeed } from '../../reducers/onboarding';
 
-export class Recover extends Component {
-  handleSubmit = async input => {
-    const { recoverSeed, navigation } = this.props;
+const Recover = () => {
+  const dispatch = useDispatch();
+  const { navigate } = useNavigation();
+  const n = [...Array(24).keys()];
+  const [phrase, setPhrasePosition] = useState(0);
+  const phraseRef = useRef(n.map(() => createRef()));
+  const [seed, setSeed] = useState([]);
 
-    const state = Object.values(input);
-    await recoverSeed(state);
-    await navigation.navigate('Pin');
+  useEffect(() => {
+    phraseRef.current[phrase].current.focus();
+  });
+
+  const handleSubmit = async index => {
+    if (index === 23) {
+      attemptLogin();
+      return;
+    }
+
+    setPhrasePosition(phrase + 1);
   };
 
-  render() {
-    const n = [...Array(24).keys()];
-    const Inputs = n.map(val => {
-      return (
-        <TextInput
-          placeholder="dummy"
-          autoCorrect={false}
-          autoCapitalize="none"
-          clearTextOnFocus
-          keyboardAppearance="dark"
-          key={val}
-        />
-      );
-    });
-    return (
-      <View>
-        <Text>Login</Text>
-        <Text>Enter your paper-key words below.</Text>
-        <Form formSubmission={this.handleSubmit}>{Inputs}</Form>
-      </View>
-    );
-  }
-}
+  const handleChange = (input, index) => {
+    if (index !== phrase) {
+      setPhrasePosition(index);
+    }
 
-const mapStateToProps = state => ({});
+    const arr = [...seed];
+    arr[index] = input;
+    setSeed(arr);
+  };
 
-const mapDispatchToProps = { recoverSeed };
+  const attemptLogin = () => {
+    dispatch(recoverSeed(seed));
+    navigate('Pin');
+  };
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(Recover);
+  return (
+    <LinearGradient colors={['#7E58FF', '#3649DF', '#003DB3']}>
+      <SafeAreaView>
+        <KeyboardAvoidingView behavior="height">
+          <View style={{ paddingTop: 50, height: '100%' }}>
+            <Text style={{ color: 'white', fontSize: 15, textAlign: 'center', paddingBottom: 25 }}>
+              Enter your paper-key words below.
+            </Text>
+            <FlatList
+              data={n}
+              renderItem={({ item, index }) => (
+                <View
+                  style={{
+                    flex: 1,
+                    flexDirection: 'row',
+                    alignItems: 'center',
+                    borderTopWidth: 1,
+                    borderColor: '#979797',
+                    height: 44,
+                    color: 'transparent'
+                  }}
+                >
+                  <Text style={{ color: '#7C96AE' }}>{index + 1}</Text>
+                  <TextInput
+                    autoCorrect={false}
+                    blurOnSubmit={false}
+                    autoCapitalize="none"
+                    clearTextOnFocus
+                    keyboardAppearance="dark"
+                    ref={phraseRef.current[index]}
+                    onSubmitEditing={() => handleSubmit(index)}
+                    onChangeText={text => handleChange(text, index)}
+                    style={{ flex: 1, height: '100%' }}
+                  />
+                </View>
+              )}
+            />
+          </View>
+        </KeyboardAvoidingView>
+      </SafeAreaView>
+    </LinearGradient>
+  );
+};
+
+Recover.navigationOptions = {
+  headerTitle: 'Login',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+    color: 'white'
+  },
+  headerTransparent: true,
+  headerBackTitle: null
+};
+
+export default Recover;
