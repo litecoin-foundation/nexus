5bbb5b35da48b50c056a4088facb112dbb195a4a
losh11 <loshan1212@gmail.com>
2019-07-12 11:10:08 +0100

    feat: add android support

--- android/app/src/main/java/com/lndmobile/MainApplication.java
+++ android/app/src/main/java/com/lndmobile/MainApplication.java
@@ -14,6 +14,7 @@ import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 import com.facebook.soloader.SoLoader;
+import com.lndmobile.LndNativePackage;
 
 import java.util.Arrays;
 import java.util.List;
@@ -36,7 +37,8 @@ public class MainApplication extends Application implements ReactApplication {
             new RNCameraPackage(),
             new RNFSPackage(),
             new RNGestureHandlerPackage(),
-            new RNSensitiveInfoPackage()
+            new RNSensitiveInfoPackage(),
+            new LndNativePackage()
       );
     }
 
