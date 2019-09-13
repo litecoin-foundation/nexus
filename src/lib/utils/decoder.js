3b2be566eec1ac8481b2cddae6ddc21c670749c9
losh11 <loshan1212@gmail.com>
2019-09-13 21:15:20 +0100

    refactor: remove unneccessary util 'decoder'

--- src/lib/utils/decoder.js
+++
@@ -1,4 +0,0 @@
-// currently lnd doesn't support sending to Litecoin P2PKH/P2SH/P2SH-P2WPKH/bech32 addresses
-// this util will convert litecoin addresses to bitcoin until lnd supports ltc addr support
-
-export const decodeAddress = address => {};
