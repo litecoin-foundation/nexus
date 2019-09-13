a088e11cd37564761a70c92b4ebd31914c99284b
losh11 <loshan1212@gmail.com>
2019-09-13 02:45:19 +0100

    refactor: bip39 functions to one file

--- src/lib/utils/getBIP39Word.js
+++
@@ -1,8 +0,0 @@
-import english from './bip39/english.json';
-import getRandomInt from './getRandomInt';
-
-const getBIP39Word = () => {
-  return english[getRandomInt(0, 2047)];
-};
-
-export default getBIP39Word;
--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,8 +1,6 @@
 export {default as toCaps} from './toCaps';
 export {default as toBuffer} from './toBuffer';
-export {default as getRandomInt} from './getRandomInt';
 export {default as randomShuffle} from './randomShuffle';
-export {default as getBIP39Word} from './getBIP39Word';
 export {default as sleep} from './sleep';
 export {default as converter} from './converter';
 export {default as groupBy} from './groupBy';
