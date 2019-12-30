29bb60de811a326e5e3e7a0f4244ba9d1cd9771e
losh11 <loshan1212@gmail.com>
2019-12-30 02:52:22 +0000

    feat: refactor Scanner as modal & complete UI

---
+++ src/components/Buttons/Switch.js
@@ -0,0 +1,45 @@
+import React, {useState} from 'react';
+import {TouchableOpacity, StyleSheet, Text, Animated} from 'react-native';
+
+const AnimatedTouchable = Animated.createAnimatedComponent(TouchableOpacity);
+
+const Switch = props => {
+  const {onPress} = props;
+  const [triggered, trigger] = useState(false);
+
+  const handlePress = () => {
+    trigger(!triggered);
+    onPress(!triggered);
+  };
+
+  return (
+    <AnimatedTouchable style={[styles.container]} onPress={() => handlePress()}>
+      <Text>{triggered ? 'ON' : 'OFF'}</Text>
+    </AnimatedTouchable>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    height: 36,
+    width: 63,
+    borderRadius: 20,
+    backgroundColor: '#D4D3DA',
+    shadowColor: 'black',
+    shadowOpacity: 0.1,
+    shadowRadius: 10,
+    shadowOffset: {
+      height: 0,
+      width: 0,
+    },
+
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  box: {
+    width: 100,
+    height: 100,
+  },
+});
+
+export default Switch;
---
+++ src/components/Modals/ScanModal.js
@@ -0,0 +1,128 @@
+import React, {useState} from 'react';
+import {
+  View,
+  StyleSheet,
+  Vibration,
+  TouchableOpacity,
+  Text,
+  Image,
+} from 'react-native';
+import Modal from 'react-native-modal';
+import {RNCamera} from 'react-native-camera';
+import LinearGradient from 'react-native-linear-gradient';
+
+import Switch from '../Buttons/Switch';
+import Header from '../Header';
+
+const ScanModal = props => {
+  const {isVisible, close, handleQRRead} = props;
+  const [flashEnabled, triggerFlash] = useState(false);
+
+  const handleRead = event => {
+    Vibration.vibrate();
+    handleQRRead(event.data);
+  };
+
+  return (
+    <Modal isVisible={isVisible} swipeDirection="down" style={styles.noMargin}>
+      <View style={styles.container}>
+        <Header>
+          <TouchableOpacity style={styles.closeButton} onPress={() => close()}>
+            <Image source={require('../../assets/images/close-white.png')} />
+          </TouchableOpacity>
+        </Header>
+
+        <RNCamera
+          style={styles.camera}
+          captureAudio={false}
+          onBarCodeRead={e => handleRead(e)}
+          type={RNCamera.Constants.Type.back}
+          barCodeTypes={[RNCamera.Constants.BarCodeType.qr]}
+          videoStabilizationMode={RNCamera.Constants.VideoStabilization.auto}
+          androidCameraPermissionOptions={{
+            title: 'Permission to use Camera',
+            message:
+              'App will use your camera to scan QR codes for Litecoin Payments',
+            buttonPositive: 'Ok',
+            buttonNegative: 'Cancel',
+          }}
+          notAuthorizedView={
+            <View>
+              <Text style={styles.textAlign}>
+                Camera not enabled. Go to Settings and enable your Camera for
+                settings to scan QR codes.
+              </Text>
+            </View>
+          }
+          flashMode={
+            flashEnabled
+              ? RNCamera.Constants.FlashMode.torch
+              : RNCamera.Constants.FlashMode.off
+          }>
+          <View style={styles.qrFrameContainer}>
+            <Image source={require('../../assets/images/qr-frame.png')} />
+          </View>
+        </RNCamera>
+        <LinearGradient
+          colors={['#7E58FFF2', '#003DB3F2']}
+          start={{x: 0, y: 0}}
+          end={{x: 1, y: 0}}
+          style={styles.bottomContainer}>
+          <View style={styles.bottomContentContainer}>
+            <Text style={styles.bottomText}>Enable Flash</Text>
+            <Switch onPress={flashStatus => triggerFlash(flashStatus)} />
+          </View>
+        </LinearGradient>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: 'white',
+  },
+  noMargin: {
+    margin: 0,
+    flex: 1,
+  },
+  camera: {
+    flex: 1,
+  },
+  bottomContainer: {
+    height: 90,
+    width: '100%',
+    justifyContent: 'center',
+    position: 'absolute',
+    bottom: 0,
+  },
+  bottomText: {
+    color: '#FFFFFF',
+    fontSize: 17,
+    fontWeight: 'bold',
+    letterSpacing: -0.39,
+  },
+  textAlign: {
+    textAlign: 'center',
+  },
+  closeButton: {
+    height: '100%',
+    justifyContent: 'center',
+    left: 15,
+  },
+  qrFrameContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    paddingBottom: 140,
+  },
+  bottomContentContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 20,
+    paddingRight: 20,
+  },
+});
+
+export default ScanModal;
