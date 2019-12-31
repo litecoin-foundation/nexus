429466144fba800f43ffa52c88a086cd11990afd
losh11 <loshan1212@gmail.com>
2019-12-31 06:34:31 +0000

    feat(ui): settings page ui wip

--- src/screens/Settings/Settings.js
+++ src/screens/Settings/Settings.js
@@ -1,25 +1,126 @@
-import React, {Component} from 'react';
-import {View, Text, TouchableOpacity} from 'react-native';
-import {connect} from 'react-redux';
+import React from 'react';
+import {
+  View,
+  Text,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions,
+  Image,
+} from 'react-native';
+import {useNavigation} from 'react-navigation-hooks';
+import LinearGradient from 'react-native-linear-gradient';
 
-export class Settings extends Component {
-  render() {
-    const {navigation} = this.props;
-    return (
-      <View>
-        <TouchableOpacity onPress={() => navigation.navigate('Channel')}>
-          <Text>Channels</Text>
+import Header from '../../components/Header';
+import BlueClearButton from '../../components/Buttons/BlueClearButton';
+
+const Settings = () => {
+  const {navigate} = useNavigation();
+
+  return (
+    <View style={styles.container}>
+      <Header />
+      <View style={styles.buttonContainer}>
+        <TouchableOpacity
+          onPress={() => navigate('General')}
+          style={styles.button}>
+          <Image
+            source={require('../../assets/images/general.png')}
+            style={styles.image}
+          />
+          <Text style={styles.buttonText}>GENERAL</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigate('Wallets')}
+          style={styles.button}>
+          <Image
+            source={require('../../assets/images/wallets.png')}
+            style={styles.image}
+          />
+          <Text style={styles.buttonText}>WALLETS</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          onPress={() => navigate('Channel')}
+          style={styles.button}>
+          <Text style={styles.buttonText}>CHANNELS</Text>
         </TouchableOpacity>
       </View>
-    );
-  }
-}
+      <View style={styles.bottomContainer}>
+        <LinearGradient
+          colors={['#F6F9FC', 'rgba(210,225,239,0)']}
+          style={styles.gradient}>
+          <View style={styles.bottomTextContainer}>
+            <Text style={styles.bottomText}>Last Signed In on Apr 20 2019</Text>
+          </View>
+          <View style={styles.bottomButtonContainer}>
+            <BlueClearButton
+              value="Sign Out"
+              onPress={() => navigate('Auth')}
+            />
+          </View>
+        </LinearGradient>
+      </View>
+    </View>
+  );
+};
 
-const mapStateToProps = state => ({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+  buttonContainer: {
+    flex: 1,
+    flexDirection: 'row',
+    flexWrap: 'wrap',
+  },
+  button: {
+    width: '50%',
+    height: Dimensions.get('window').width / 2,
+    aspectRatio: 1,
+    borderColor: '#979797',
+    borderWidth: 0.5,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  buttonText: {
+    color: '#7C96AE',
+    fontSize: 12,
+    fontWeight: '600',
+  },
+  image: {
+    marginBottom: 15,
+  },
+  bottomContainer: {
+    bottom: 0,
+    height: 135,
+  },
+  gradient: {
+    flex: 1,
+  },
+  bottomText: {
+    opacity: 0.9,
+    color: '#7C96AE',
+    fontSize: 11,
+    fontWeight: '500',
+  },
+  bottomTextContainer: {
+    flex: 1,
+    justifyContent: 'center',
+    paddingLeft: 40,
+  },
+  bottomButtonContainer: {
+    flex: 1,
+    alignItems: 'center',
+  },
+});
 
-const mapDispatchToProps = {};
+Settings.navigationOptions = {
+  headerTitle: 'Settings',
+  headerTitleStyle: {
+    fontWeight: 'bold',
+    color: 'white',
+  },
+  headerTransparent: true,
+  headerBackTitle: null,
+};
 
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps,
-)(Settings);
+export default Settings;
