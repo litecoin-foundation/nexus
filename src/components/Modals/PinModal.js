ff8bd642ebd9865ce9b1b01d35673c2b3609406a
losh11 <loshan1212@gmail.com>
2020-05-21 07:39:58 +0100

    feat: auth modal in settings page

---
+++ src/components/Modals/PinModal.js
@@ -0,0 +1,99 @@
+import React from 'react';
+import {View, Text, StyleSheet} from 'react-native';
+import Modal from 'react-native-modal';
+import {Pagination} from 'react-native-snap-carousel';
+import {useSelector} from 'react-redux';
+import LinearGradient from 'react-native-linear-gradient';
+
+import AuthPad from '../Numpad/AuthPad';
+import GreyRoundButton from '../Buttons/GreyRoundButton';
+
+const PinModal = (props) => {
+  const {
+    isVisible,
+    close,
+    handleValidationFailure,
+    handleValidationSuccess,
+  } = props;
+  const pin = useSelector((state) => state.authpad.pin);
+
+  return (
+    <Modal
+      isVisible={isVisible}
+      swipeDirection="down"
+      onSwipeComplete={() => close()}
+      onBackdropPress={() => close()}
+      backdropColor="rgb(19,58,138)"
+      backdropOpacity={0.6}
+      style={styles.noMargin}>
+      <View style={styles.container}>
+        <View style={styles.modal}>
+          <View style={styles.modalHeaderContainer}>
+            <Text style={styles.modalHeaderTitle}>Enter Pincode</Text>
+            <GreyRoundButton onPress={() => close()} />
+          </View>
+          <LinearGradient
+            style={styles.contentContainer}
+            colors={['#F2F8FD', '#d2e1ef00']}>
+            <Pagination
+              dotStyle={styles.dotStyle}
+              inactiveDotColor="#2C72FF"
+              dotColor="#2C72FF"
+              dotsLength={6}
+              activeDotIndex={pin.length - 1}
+            />
+            <AuthPad
+              handleValidationFailure={handleValidationFailure}
+              handleValidationSuccess={handleValidationSuccess}
+            />
+          </LinearGradient>
+        </View>
+      </View>
+    </Modal>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    justifyContent: 'flex-end',
+    margin: 0,
+  },
+  modal: {
+    backgroundColor: 'white',
+    height: 600,
+    width: '100%',
+    borderTopLeftRadius: 30,
+    borderTopRightRadius: 30,
+    position: 'absolute',
+    bottom: 0,
+  },
+  modalHeaderContainer: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    paddingLeft: 25,
+    paddingRight: 25,
+    paddingTop: 25,
+    paddingBottom: 25,
+  },
+  modalHeaderTitle: {
+    color: '#4E6070',
+    fontSize: 26,
+    fontWeight: 'bold',
+    height: 31,
+  },
+  noMargin: {
+    margin: 0,
+  },
+  dotStyle: {
+    width: 9,
+    height: 9,
+    borderRadius: 9 / 2,
+    marginHorizontal: 5,
+  },
+  contentContainer: {
+    flex: 1,
+  },
+});
+
+export default PinModal;
--- src/screens/Settings/General.js
+++ src/screens/Settings/General.js
@@ -1,5 +1,5 @@
-import React from 'react';
-import {ScrollView, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {ScrollView, StyleSheet, DeviceEventEmitter, Alert} from 'react-native';
 import {useSelector} from 'react-redux';
 import LinearGradient from 'react-native-linear-gradient';
 import {useDispatch} from 'react-redux';
@@ -7,9 +7,12 @@ import {useDispatch} from 'react-redux';
 import Header from '../../components/Header';
 import SettingCell from '../../components/Cells/SettingCell';
 import {setBiometricEnabled} from '../../reducers/authentication';
+import PinModal from '../../components/Modals/PinModal';
 
 const General = (props) => {
   const dispatch = useDispatch();
+  const [isPinModalTriggered, triggerPinModal] = useState(false);
+
   const biometricsAvailable = useSelector(
     (state) => state.authentication.biometricsAvailable,
   );
@@ -24,31 +27,72 @@ const General = (props) => {
     dispatch(setBiometricEnabled(!biometricsEnabled));
   };
 
+  // TODO: okay I realise I shouldn't be using DeviceEventEmitter
+  // need to figure out a better way in the future
+  const handleAuthenticationRequired = () => {
+    return new Promise((resolve, reject) => {
+      triggerPinModal(true);
+      const subscription = DeviceEventEmitter.addListener('auth', (bool) => {
+        if (bool === true) {
+          triggerPinModal(false);
+          subscription.remove();
+          resolve();
+        } else if (bool === false) {
+          subscription.remove();
+          reject();
+        }
+      });
+    });
+  };
+
   return (
-    <LinearGradient style={styles.container} colors={['#F2F8FD', '#d2e1ef00']}>
-      <Header />
-      <ScrollView>
-        <SettingCell title="About" />
-        <SettingCell
-          title="Change Wallet Pin"
-          onPress={() => props.navigation.navigate('ChangePincode')}
-          forward
-        />
-        {biometricsAvailable ? (
+    <>
+      <LinearGradient
+        style={styles.container}
+        colors={['#F2F8FD', '#d2e1ef00']}>
+        <Header />
+        <ScrollView>
+          <SettingCell title="About" />
           <SettingCell
-            title={`Enable ${faceIDSupported ? 'Face ID' : 'Touch ID'}`}
-            switchEnabled
-            switchValue={biometricsEnabled}
-            handleSwitch={handleBiometricSwitch}
+            title="Change Wallet Pin"
+            onPress={() => props.navigation.navigate('ChangePincode')}
+            forward
           />
-        ) : null}
-        <SettingCell
-          title="View Paper Key"
-          onPress={() => props.navigation.navigate('ChangePincode')}
-          forward
-        />
-      </ScrollView>
-    </LinearGradient>
+          {biometricsAvailable ? (
+            <SettingCell
+              title={`Enable ${faceIDSupported ? 'Face ID' : 'Touch ID'}`}
+              switchEnabled
+              switchValue={biometricsEnabled}
+              handleSwitch={handleBiometricSwitch}
+            />
+          ) : null}
+          <SettingCell
+            title="View Paper Key"
+            onPress={() => {
+              handleAuthenticationRequired()
+                .then(() => props.navigation.navigate('Seed'))
+                .catch(() =>
+                  Alert.alert('Incorrect Pincode', null, [
+                    {
+                      text: 'Dismiss',
+                      onPress: () => triggerPinModal(false),
+                      style: 'cancel',
+                    },
+                  ]),
+                );
+            }}
+            forward
+          />
+        </ScrollView>
+      </LinearGradient>
+
+      <PinModal
+        isVisible={isPinModalTriggered}
+        close={() => triggerPinModal(false)}
+        handleValidationFailure={() => DeviceEventEmitter.emit('auth', false)}
+        handleValidationSuccess={() => DeviceEventEmitter.emit('auth', true)}
+      />
+    </>
   );
 };
 
