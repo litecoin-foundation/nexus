2a9b90d3417d46613219c59a58a9fd6b50e3184e
losh11 <loshan1212@gmail.com>
2020-08-01 09:38:10 +0100

    feat: style Welcome screen

--- src/screens/Onboarding/Welcome.js
+++ src/screens/Onboarding/Welcome.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {View, StyleSheet} from 'react-native';
+import {StyleSheet, Text} from 'react-native';
 import {useDispatch} from 'react-redux';
+import LinearGradient from 'react-native-linear-gradient';
 
-import BlueButton from '../../components/Buttons/BlueButton';
+import WhiteButton from '../../components/Buttons/WhiteButton';
 import {initWallet} from '../../reducers/lightning';
 
 const Welcome = (props) => {
@@ -14,18 +15,42 @@ const Welcome = (props) => {
   };
 
   return (
-    <View style={styles.container}>
-      <BlueButton value="Welcome" onPress={handlePress} />
-    </View>
+    <LinearGradient colors={['#544FE6', '#1c44b4']} style={styles.container}>
+      <Text style={styles.text}>Welcome!</Text>
+      <WhiteButton
+        value="Tap Anywhere to Start"
+        small={false}
+        onPress={() => handlePress()}
+        active={true}
+      />
+    </LinearGradient>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignContent: 'center',
-    justifyContent: 'center',
+    justifyContent: 'flex-end',
+    alignItems: 'center',
+    paddingBottom: 50,
+  },
+  text: {
+    color: 'white',
+    fontSize: 28,
+    fontWeight: 'bold',
+    letterSpacing: -0.18,
+    lineHeight: 34,
+    paddingBottom: 556,
+    textShadowColor: 'rgba(0, 0, 0, 0.11)',
+    textShadowOffset: {width: 0, height: 3},
+    textShadowRadius: 2,
   },
 });
+
+Welcome.navigationOptions = {
+  headerTransparent: true,
+  headerBackTitleVisible: false,
+  headerShown: false,
+};
 
 export default Welcome;
