935e6c885a0dcca2cb659d10879951b45bb05a70
Mohamed Barry <mosadialiou@gmail.com>
2022-06-03 03:55:22 +0100

    feat: lndltc android integration

--- android/app/src/main/java/com/litecoin/plasma/newarchitecture/MainApplicationReactNativeHost.java
+++ android/app/src/main/java/com/litecoin/plasma/newarchitecture/MainApplicationReactNativeHost.java
@@ -1,4 +1,4 @@
-package com.plasma.newarchitecture;
+package com.litecoin.plasma.newarchitecture;
 
 import android.app.Application;
 import androidx.annotation.NonNull;
@@ -19,9 +19,9 @@ import com.facebook.react.fabric.CoreComponentsRegistry;
 import com.facebook.react.fabric.EmptyReactNativeConfig;
 import com.facebook.react.fabric.FabricJSIModuleProvider;
 import com.facebook.react.uimanager.ViewManagerRegistry;
-import com.plasma.BuildConfig;
-import com.plasma.newarchitecture.components.MainComponentsRegistry;
-import com.plasma.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
+import com.litecoin.plasma.BuildConfig;
+import com.litecoin.plasma.newarchitecture.components.MainComponentsRegistry;
+import com.litecoin.plasma.newarchitecture.modules.MainApplicationTurboModuleManagerDelegate;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -37,13 +37,11 @@ public class MainApplicationReactNativeHost extends ReactNativeHost {
     super(application);
   }
 
-  @Override
-  public boolean getUseDeveloperSupport() {
+  @Override public boolean getUseDeveloperSupport() {
     return BuildConfig.DEBUG;
   }
 
-  @Override
-  protected List<ReactPackage> getPackages() {
+  @Override protected List<ReactPackage> getPackages() {
     List<ReactPackage> packages = new PackageList(this).getPackages();
     // Packages that cannot be autolinked yet can be added manually here, for example:
     //     packages.add(new MyReactNativePackage());
@@ -54,61 +52,47 @@ public class MainApplicationReactNativeHost extends ReactNativeHost {
     return packages;
   }
 
-  @Override
-  protected String getJSMainModuleName() {
+  @Override protected String getJSMainModuleName() {
     return "index";
   }
 
-  @NonNull
-  @Override
-  protected ReactPackageTurboModuleManagerDelegate.Builder
-      getReactPackageTurboModuleManagerDelegateBuilder() {
+  @NonNull @Override protected ReactPackageTurboModuleManagerDelegate.Builder getReactPackageTurboModuleManagerDelegateBuilder() {
     // Here we provide the ReactPackageTurboModuleManagerDelegate Builder. This is necessary
     // for the new architecture and to use TurboModules correctly.
     return new MainApplicationTurboModuleManagerDelegate.Builder();
   }
 
-  @Override
-  protected JSIModulePackage getJSIModulePackage() {
+  @Override protected JSIModulePackage getJSIModulePackage() {
     return new JSIModulePackage() {
-      @Override
-      public List<JSIModuleSpec> getJSIModules(
-          final ReactApplicationContext reactApplicationContext,
+      @Override public List<JSIModuleSpec> getJSIModules(final ReactApplicationContext reactApplicationContext,
           final JavaScriptContextHolder jsContext) {
         final List<JSIModuleSpec> specs = new ArrayList<>();
 
         // Here we provide a new JSIModuleSpec that will be responsible of providing the
         // custom Fabric Components.
-        specs.add(
-            new JSIModuleSpec() {
-              @Override
-              public JSIModuleType getJSIModuleType() {
-                return JSIModuleType.UIManager;
-              }
+        specs.add(new JSIModuleSpec() {
+          @Override public JSIModuleType getJSIModuleType() {
+            return JSIModuleType.UIManager;
+          }
 
-              @Override
-              public JSIModuleProvider<UIManager> getJSIModuleProvider() {
-                final ComponentFactory componentFactory = new ComponentFactory();
-                CoreComponentsRegistry.register(componentFactory);
+          @Override public JSIModuleProvider<UIManager> getJSIModuleProvider() {
+            final ComponentFactory componentFactory = new ComponentFactory();
+            CoreComponentsRegistry.register(componentFactory);
 
-                // Here we register a Components Registry.
-                // The one that is generated with the template contains no components
-                // and just provides you the one from React Native core.
-                MainComponentsRegistry.register(componentFactory);
+            // Here we register a Components Registry.
+            // The one that is generated with the template contains no components
+            // and just provides you the one from React Native core.
+            MainComponentsRegistry.register(componentFactory);
 
-                final ReactInstanceManager reactInstanceManager = getReactInstanceManager();
+            final ReactInstanceManager reactInstanceManager = getReactInstanceManager();
 
-                ViewManagerRegistry viewManagerRegistry =
-                    new ViewManagerRegistry(
-                        reactInstanceManager.getOrCreateViewManagers(reactApplicationContext));
+            ViewManagerRegistry viewManagerRegistry =
+                new ViewManagerRegistry(reactInstanceManager.getOrCreateViewManagers(reactApplicationContext));
 
-                return new FabricJSIModuleProvider(
-                    reactApplicationContext,
-                    componentFactory,
-                    new EmptyReactNativeConfig(),
-                    viewManagerRegistry);
-              }
-            });
+            return new FabricJSIModuleProvider(reactApplicationContext, componentFactory, new EmptyReactNativeConfig(),
+                viewManagerRegistry);
+          }
+        });
         return specs;
       }
     };
