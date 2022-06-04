51b5fbc3fa12ef9f46a7c4fcea993e303e90ce0f
losh11 <loshan1212@gmail.com>
2022-06-04 20:45:39 +0100

    refactor: remove unused code

--- src/lib/utils/index.js
+++ src/lib/utils/index.js
@@ -1,5 +1,3 @@
-export {default as toCaps} from './toCaps';
-export {default as toBuffer} from './toBuffer';
 export {default as randomShuffle} from './randomShuffle';
 export {default as groupBy} from './groupBy';
 export {default as validate} from './validate';
--- src/lib/utils/toCaps.js
+++
@@ -1,8 +0,0 @@
-const toCaps = (value = '', separator = ' ', split = '-') => {
-  return value
-    .split(split)
-    .map((v) => v.charAt(0).toUpperCase() + v.substring(1))
-    .reduce((a, b) => `${a}${separator}${b}`);
-};
-
-export default toCaps;
