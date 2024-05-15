442143eca0fe268b7c064e6d6ef83ab4528b85b4
Loshan T <loshan1212@gmail.com>
2024-05-15 14:53:52 +0100

    build(dep): rn 0.74.1

--- App.tsx
+++ App.tsx
@@ -1,12 +1,11 @@
-/* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {StatusBar, Platform} from 'react-native';
+import {Platform, StatusBar} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
-import {store, pStore} from './src/store';
 
 import RootNavigator from './src/navigation/RootNavigator';
+import {store, pStore} from './src/store';
 
 type RootStackParamList = {
   Scan: {
@@ -20,7 +19,7 @@ declare global {
   }
 }
 
-const App = () => (
+const App: React.FC = () => (
   <>
     <Provider store={store}>
       {Platform.OS === 'android' ? (
