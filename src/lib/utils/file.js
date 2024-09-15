f7a97102497f717aa6791572e979ab468d04302a
Loshan T <loshan1212@gmail.com>
2024-09-15 02:57:53 +0100

    build: rn 0.75.3 + remove rnfs + newarch enabled

--- src/lib/utils/file.js
+++ src/lib/utils/file.js
@@ -1,16 +1,22 @@
-import RNFS from '@dr.pogodin/react-native-fs';
+import * as FileSystem from 'expo-file-system';
 
-const lndDir = RNFS.DocumentDirectoryPath;
-const externalStorageDir = RNFS.ExternalStorageDirectoryPath;
-const scbPath = `${lndDir}/lndltc/data/chain/litecoin/mainnet/channel.backup`;
-const scbExternalDir = `${externalStorageDir}/Lightning/mainnet-ltc`;
-const scbExternalPath = `${scbExternalDir}/channel.backup`;
+const lndDir = FileSystem.documentDirectory;
+// const externalStorageDir = RNFS.ExternalStorageDirectoryPath;
+// const scbPath = `${lndDir}/lndltc/data/chain/litecoin/mainnet/channel.backup`;
+// const scbExternalDir = `${externalStorageDir}/Lightning/mainnet-ltc`;
+// const scbExternalPath = `${scbExternalDir}/channel.backup`;
+
+// tools
+export const fileExists = async path => {
+  const {exists} = await FileSystem.getInfoAsync(path);
+  return exists;
+};
 
 // wallet database
 export const deleteWalletDB = async () => {
   const dbPath = `${lndDir}/lndltc/data/chain/litecoin/mainnet/wallet.db`;
   try {
-    await RNFS.unlink(dbPath);
+    await FileSystem.deleteAsync(dbPath);
   } catch (error) {
     // if initial install, then no wallet db will exist
     console.log('no wallet db exists');
@@ -21,7 +27,7 @@ export const deleteWalletDB = async () => {
 export const deleteLNDDir = async () => {
   const path = `${lndDir}/lndltc`;
   try {
-    await RNFS.unlink(path);
+    await FileSystem.deleteAsync(path);
   } catch (error) {
     console.error(error);
   }
@@ -29,25 +35,30 @@ export const deleteLNDDir = async () => {
 
 // static channel backups
 export const readSCB = async () => {
-  return RNFS.readFile(
-    `${lndDir}/lndltc/data/chain/litecoin/mainnet/channel.backup`,
-    'base64',
-  );
+  try {
+    const scb = await FileSystem.readAsStringAsync(
+      `${lndDir}/lndltc/data/chain/litecoin/mainnet/channel.backup`,
+      {encoding: 'base64'},
+    );
+    return scb;
+  } catch (error) {
+    console.error(error);
+  }
 };
 
-export const copySCBToExternalStorage = async () => {
-  const exists = await RNFS.exists(scbPath);
-  if (!exists) {
-    return;
-  }
-  await RNFS.mkdir(scbExternalDir);
-  await RNFS.copyFile(scbPath, scbExternalPath);
-};
+// export const copySCBToExternalStorage = async () => {
+//   const exists = await fileExists(scbPath);
+//   if (!exists) {
+//     return;
+//   }
+//   await RNFS.mkdir(scbExternalDir);
+//   await RNFS.copyFile(scbPath, scbExternalPath);
+// };
 
-export const readSCBFromExternalStorage = async () => {
-  const exists = await RNFS.exists(scbExternalPath);
-  if (!exists) {
-    return;
-  }
-  return RNFS.readFile(scbExternalPath, 'base64');
-};
+// export const readSCBFromExternalStorage = async () => {
+//   const exists = await fileExists(scbExternalPath);
+//   if (!exists) {
+//     return;
+//   }
+//   return RNFS.readFile(scbExternalPath, 'base64');
+// };
