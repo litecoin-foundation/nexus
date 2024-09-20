088fc6c1bfa3af612afcd93cce7b33f0d4a88d3f
Loshan T <loshan1212@gmail.com>
2024-09-20 10:05:38 +0100

    feat: global error ui

--- src/screens/Settings/Import.tsx
+++ src/screens/Settings/Import.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {StyleSheet, View, SafeAreaView} from 'react-native';
+import {StyleSheet, View, SafeAreaView, Text, Platform} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 
 import Card from '../../components/Card';
@@ -9,6 +9,8 @@ import {RouteProp} from '@react-navigation/native';
 import {sweepWIF} from '../../lib/utils/sweep';
 import {useAppDispatch, useAppSelector} from '../../store/hooks';
 import {getAddress} from '../../reducers/address';
+import {showError} from '../../reducers/errors';
+import HeaderButton from '../../components/Buttons/HeaderButton';
 
 type RootStackParamList = {
   Import: {
@@ -34,19 +36,24 @@ const Import: React.FC<Props> = props => {
   // handle scanned QR code
   useEffect(() => {
     if (route.params?.scanData) {
-      console.warn(route.params?.scanData);
-      sweepWIF(route.params.scanData, address);
+      console.log(route.params?.scanData);
+      sweepWIF(route.params.scanData, address)
+        .then(rawTx => {
+          console.log('successfully created raw tx, time to broadcast!');
+          console.log(rawTx);
+        })
+        .catch(error => dispatch(showError(String(error))));
     }
-  }, [address, route.params?.scanData]);
+  }, [address, route.params?.scanData, dispatch]);
 
   return (
-    <LinearGradient colors={['#544FE6', '#1c44b4']} style={styles.container}>
+    <LinearGradient colors={['#1162E6', '#0F55C7']} style={styles.container}>
       <SafeAreaView />
 
       <Card
         titleText="Import Private Key"
         descriptionText={
-          'Importing a Litecoin private key transfers all the Litecoin from that wallet into Plasma.\n\nYou will no longer be able to access Litecoin using this private key.'
+          'Importing a Litecoin private key moves any Litecoin in that wallet into Plasma.\n\nYou will no longer be able to access Litecoin using the private key.'
         }
         imageSource={require('../../assets/images/qr-frame.png')}
       />
@@ -57,11 +64,7 @@ const Import: React.FC<Props> = props => {
           small={false}
           active={true}
           onPress={() => {
-            // navigation.navigate('Scan', {returnRoute: 'Import'});
-            sweepWIF(
-              'T9AGGdbPB8awSweFqjnmhTnLLLwfoZvDgqvZq74S3QqnudmGUfvg',
-              address,
-            );
+            navigation.navigate('Scan', {returnRoute: 'Import'});
           }}
         />
       </View>
@@ -75,6 +78,33 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'space-around',
   },
+  headerTitle: {
+    fontFamily:
+      Platform.OS === 'ios'
+        ? 'Satoshi Variable'
+        : 'SatoshiVariable-Regular.ttf',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    color: 'white',
+    fontSize: 17,
+  },
 });
+
+export const ImportNavigationOptions = navigation => {
+  return {
+    headerTitle: () => (
+      <Text style={styles.headerTitle}>Import Private Key</Text>
+    ),
+    headerTitleAlign: 'left',
+    headerTransparent: true,
+    headerTintColor: 'white',
+    headerLeft: () => (
+      <HeaderButton
+        onPress={() => navigation.goBack()}
+        imageSource={require('../../assets/images/back-icon.png')}
+      />
+    ),
+  };
+};
 
 export default Import;
