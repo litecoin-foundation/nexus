449c53f854c6fbd602b65b21a236774ad8d6b39f
Victor Ogorodnov <vitek984@gmail.com>
2024-10-03 21:27:08 +0500

    feat(wip): Wallets Modal UI

---
+++ src/components/Buttons/ChooseWalletButton.tsx
@@ -0,0 +1,132 @@
+import React from 'react';
+import {View, TouchableOpacity, Text, StyleSheet, Platform, Dimensions, Image} from 'react-native';
+import Svg, {Path} from 'react-native-svg';
+
+interface Props {
+  value: string;
+  onPress(): void;
+  disabled?: boolean;
+  customStyles?: {};
+  customFontStyles?: {};
+  isBottomCurvesEnabled?: boolean;
+  isModalOpened?: boolean;
+}
+
+const ChooseWalletButton: React.FC<Props> = props => {
+  const {
+    value,
+    onPress,
+    disabled,
+    customStyles,
+    customFontStyles,
+    isBottomCurvesEnabled,
+    isModalOpened,
+  } = props;
+
+  const styles = StyleSheet.create({
+    container: {
+      height: Dimensions.get('screen').height * 0.035,
+      width: '100%',
+      backgroundColor: 'transparent',
+      justifyContent: 'center',
+      alignItems: 'center',
+    },
+    buttonBox: {
+      height: '100%',
+      borderRadius: Dimensions.get('screen').height * 0.01,
+      // backgroundColor: 'red',
+      backgroundColor: '#0d3d8a',
+      flexDirection: 'row',
+      alignItems: 'center',
+      paddingLeft: Dimensions.get('screen').height * 0.015,
+      paddingRight: Dimensions.get('screen').height * 0.015,
+      position: 'relative',
+    },
+    boxText: {
+      fontFamily:
+      Platform.OS === 'ios'
+        ? 'Satoshi Variable'
+        : 'SatoshiVariable-Regular.ttf',
+      color: '#fff',
+      fontStyle: 'normal',
+      fontWeight: '500',
+      fontSize: 15,
+    },
+    boxArrow: {
+      height: Dimensions.get('screen').height * 0.012,
+      alignItems: 'center',
+      justifyContent: 'center',
+      transform: isModalOpened ? 'rotate(90deg)' : 'rotate(-90deg)',
+      objectFit: 'contain',
+      marginTop: 2,
+      marginLeft: 10,
+    },
+    boxLeftCurve: {
+      position: 'absolute',
+      bottom: 0,
+      left: Dimensions.get('screen').height * 0.02 * -1,
+      height: Dimensions.get('screen').height * 0.02,
+      width: Dimensions.get('screen').height * 0.02,
+    },
+    boxRightCurve: {
+      position: 'absolute',
+      bottom: 0,
+      right: Dimensions.get('screen').height * 0.02 * -1,
+      height: Dimensions.get('screen').height * 0.02,
+      width: Dimensions.get('screen').height * 0.02,
+    },
+    disabled: {
+      opacity: 0.5,
+    },
+  });
+
+  return (
+    <TouchableOpacity
+      activeOpacity={1}
+      disabled={disabled}
+      style={[
+        styles.container,
+        disabled ? styles.disabled : null,
+        customStyles,
+      ]}
+      onPress={onPress}>
+      <View style={[styles.buttonBox, customStyles]}>
+        <Text
+          style={[
+            styles.boxText,
+            customFontStyles,
+          ]}>
+          {value}
+        </Text>
+        <Image style={styles.boxArrow} source={require('../../assets/images/back-icon.png')} />
+        {isBottomCurvesEnabled ? (
+          <>
+            <Svg
+              style={styles.boxLeftCurve}
+              viewBox="0 0 15 15"
+              fill="#0d3d8a"
+            >
+              <Path
+                d="M 0 15 q 15 0 15 -15 l 0 15"
+              />
+            </Svg>
+            <Svg
+              style={styles.boxRightCurve}
+              viewBox="0 0 15 15"
+              fill="#0d3d8a"
+            >
+              <Path
+                d="M 0 0 q 0 15 15 15 l -15 0"
+              />
+            </Svg>
+          </>
+        ) : (
+          <>
+          </>
+        )}
+      </View>
+    </TouchableOpacity>
+  );
+};
+
+export default ChooseWalletButton;
