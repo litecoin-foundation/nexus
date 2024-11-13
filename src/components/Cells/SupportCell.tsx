0714e9962e6a5cfefa686344f3d7208eb4064ead
Loshan T <loshan1212@gmail.com>
2024-11-13 07:47:13 +0000

    feat(wip): support live chat

---
+++ src/components/Cells/SupportCell.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import {Platform, StyleSheet, Text, TouchableOpacity, View} from 'react-native';
+
+interface Props {
+  onPress: () => void;
+}
+
+const SupportCell: React.FC<Props> = props => {
+  const {onPress} = props;
+  return (
+    <TouchableOpacity style={styles.container} onPress={() => onPress()}>
+      <View style={styles.questionBox}>
+        <Text style={styles.questionText}>?</Text>
+      </View>
+      <Text style={styles.title}>Need Support? Ask a question!</Text>
+    </TouchableOpacity>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    flexDirection: 'row',
+    gap: 11,
+    alignItems: 'center',
+    paddingLeft: 25,
+    paddingRight: 25,
+    height: 66,
+    backgroundColor: 'white',
+    marginBottom: 27,
+  },
+  title: {
+    fontFamily:
+      Platform.OS === 'ios'
+        ? 'Satoshi Variable'
+        : 'SatoshiVariable-Regular.ttf',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    color: '#484859',
+    fontSize: 16,
+  },
+  questionBox: {
+    height: 30,
+    width: 30,
+    borderRadius: 9,
+    backgroundColor: '#1162E6',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+  questionText: {
+    fontFamily:
+      Platform.OS === 'ios'
+        ? 'Satoshi Variable'
+        : 'SatoshiVariable-Regular.ttf',
+    fontStyle: 'normal',
+    fontWeight: '700',
+    color: 'white',
+    fontSize: 15,
+  },
+});
+
+export default SupportCell;
