d8fc0a014eead35d5f30b841e2b9551bf0828e48
Victor Ogorodnov <vitek984@gmail.com>
2024-11-15 18:12:32 +0500

    refactor: alerts

--- App.tsx
+++ App.tsx
@@ -1,8 +1,18 @@
-import React from 'react';
+import React, { useLayoutEffect, useState } from 'react';
 import {Platform, StatusBar} from 'react-native';
 import {GestureHandlerRootView} from 'react-native-gesture-handler';
 import {Provider} from 'react-redux';
 import {PersistGate} from 'redux-persist/integration/react';
+import {
+  Notifications,
+  Registered,
+  RegistrationError,
+  Notification,
+  NotificationCompletion,
+  NotificationBackgroundFetchResult,
+} from 'react-native-notifications';
+import {useAppDispatch} from './src/store/hooks';
+import {setDeviceNotificationToken} from './src/reducers/settings';
 
 import RootNavigator from './src/navigation/RootNavigator';
 import {store, pStore} from './src/store';
@@ -20,20 +30,69 @@ declare global {
   }
 }
 
-const App: React.FC = () => (
-  <>
+function ContextExecutable(props: any) {
+  const dispatch = useAppDispatch();
+  dispatch(setDeviceNotificationToken(props.deviceToken));
+  return <></>;
+}
+
+const App: React.FC = () => {
+
+  const [deviceToken, setDeviceToken] = useState('');
+
+  useLayoutEffect(() => {
+    Notifications.registerRemoteNotifications();
+
+    Notifications.events().registerRemoteNotificationsRegistered((event: Registered) => {
+        // TODO: Send the token to my server so it could send back push notifications...
+        console.log('Device Token Received', event.deviceToken);
+        setDeviceToken(event.deviceToken);
+    });
+    Notifications.events().registerRemoteNotificationsRegistrationFailed((event: RegistrationError) => {
+        // console.error(event);
+    });
+
+    Notifications.events().registerNotificationReceivedForeground((notification: Notification, completion: (response: NotificationCompletion) => void) => {
+      // console.log('Notification Received - Foreground', notification.payload);
+
+      // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
+      completion({alert: true, sound: true, badge: false});
+    });
+
+    Notifications.events().registerNotificationOpened((notification: Notification, completion: () => void, action: any) => {
+      // console.log('Notification opened by device user', notification.payload);
+      // console.log(`Notification opened with an action identifier: ${action.identifier} and response text: ${action.text}`);
+      completion();
+    });
+
+    Notifications.events().registerNotificationReceivedBackground((notification: Notification, completion: (response: NotificationBackgroundFetchResult) => void) => {
+      // console.log('Notification Received - Background', notification.payload);
+
+      // Calling completion on iOS with `alert: true` will present the native iOS inApp notification.
+      completion({alert: true, sound: true, badge: false});
+    });
+
+    Notifications.getInitialNotification()
+      .then((notification) => {
+        // console.log('Initial notification was:', (notification ? notification.payload : 'N/A'));
+        })
+      .catch((err) => console.error('getInitialNotifiation() failed', err));
+  }, []);
+
+  return <>
     <Provider store={store}>
       {Platform.OS === 'android' ? (
         <StatusBar hidden={true} backgroundColor="transparent" />
       ) : null}
       <PersistGate loading={null} persistor={pStore}>
+        <ContextExecutable deviceToken={deviceToken}/>
         <GestureHandlerRootView style={{flex: 1}}>
-          <RootNavigator />
+          <RootNavigator deviceToken={deviceToken} />
           <Error />
         </GestureHandlerRootView>
       </PersistGate>
     </Provider>
-  </>
-);
+  </>;
+};
 
 export default App;
