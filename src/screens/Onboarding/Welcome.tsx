c75e1ac456642070eecb6033d6103890197d259a
losh11 <loshan1212@gmail.com>
2025-01-10 12:17:25 +0000

    fix: initWallet blocking thread

--- src/screens/Onboarding/Welcome.tsx
+++ src/screens/Onboarding/Welcome.tsx
@@ -1,5 +1,5 @@
 import React, {useEffect} from 'react';
-import {SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import {StyleSheet, Text, View} from 'react-native';
 import LinearGradient from 'react-native-linear-gradient';
 import {StackNavigationProp} from '@react-navigation/stack';
 
