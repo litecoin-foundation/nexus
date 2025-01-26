83eef7321b0e121ff70703d83c51bf1f6e4966ae
Victor Ogorodnov <vitek984@gmail.com>
2025-01-26 21:31:24 +0500

    style: FilterButton animation

--- src/components/Buttons/FilterButton.tsx
+++ src/components/Buttons/FilterButton.tsx
@@ -1,4 +1,4 @@
-import React, {useContext} from 'react';
+import React, {useContext, useEffect} from 'react';
 import {
   Image,
   ImageSourcePropType,
@@ -6,7 +6,12 @@ import {
   StyleSheet,
   Text,
 } from 'react-native';
-
+import Animated, {
+  interpolateColor,
+  useAnimatedStyle,
+  useSharedValue,
+  withSpring,
+} from 'react-native-reanimated';
 import {ScreenSizeContext} from '../../context/screenSize';
 
 interface Props {
@@ -24,17 +29,50 @@ const FilterButton: React.FC<Props> = props => {
     useContext(ScreenSizeContext);
   const styles = getStyles(SCREEN_WIDTH, SCREEN_HEIGHT, tint);
 
+  const scaleValue = useSharedValue(1);
+  const backgroundValue = useSharedValue(active ? 2 : 0);
+
+  const motionStyle = useAnimatedStyle(() => {
+    const bgIterpolation = interpolateColor(
+      backgroundValue.value,
+      [0, 1, 2],
+      ['transparent', '#0a429b80', '#0a429b'],
+    );
+
+    return {
+      transform: [{scale: scaleValue.value}],
+      backgroundColor: bgIterpolation,
+    };
+  });
+
+  const onPressIn = () => {
+    scaleValue.value = withSpring(0.9, {mass: 1});
+    backgroundValue.value = withSpring(1, {mass: 1});
+  };
+
+  const onPressOut = () => {
+    scaleValue.value = withSpring(1, {mass: 0.7});
+    backgroundValue.value = 2;
+    onPress();
+  };
+
+  useEffect(() => {
+    if (!active) {
+      backgroundValue.value = 0;
+    }
+  }, [active, backgroundValue]);
+
   return (
-    <Pressable
-      style={[styles.button, active ? styles.activeButton : null]}
-      onPress={onPress}>
-      <Image style={styles.image} source={imageSource} />
-      <Text style={styles.text}>{title}</Text>
+    <Pressable onPressIn={onPressIn} onPressOut={onPressOut}>
+      <Animated.View style={[styles.button, motionStyle]}>
+        <Image style={[styles.image, motionStyle]} source={imageSource} />
+        <Text style={styles.text}>{title}</Text>
+      </Animated.View>
     </Pressable>
   );
 };
 
-const getStyles = (screenWidth: number, screenHeight: number, tint: boolean) =>
+const getStyles = (screenWidth: number, screenHeight: number, tint?: boolean) =>
   StyleSheet.create({
     button: {
       height: screenHeight * 0.065,
@@ -45,9 +83,6 @@ const getStyles = (screenWidth: number, screenHeight: number, tint: boolean) =>
       alignItems: 'center',
       justifyContent: 'space-around',
       paddingVertical: screenHeight * 0.01,
-    },
-    activeButton: {
-      backgroundColor: '#0A429B',
     },
     text: {
       fontFamily: 'Satoshi Variable',
