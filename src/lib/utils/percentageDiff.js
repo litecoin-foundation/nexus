9eff9686b41aed454c31c5fb2a3db530f405f368
Victor Ogorodnov <vitek984@gmail.com>
2025-03-15 02:46:49 +0500

    fix: percentage change

--- src/lib/utils/percentageDiff.js
+++ src/lib/utils/percentageDiff.js
@@ -1,5 +1,5 @@
 const percentageDiff = (v1, v2) => {
-  const difference = v1 - v2;
+  const difference = v2 - v1;
   const sign = Math.sign(difference);
   const relativeDifference = 100 * Math.abs(difference / ((v1 + v2) / 2));
   const roundedDifference = Math.round(relativeDifference * 10) / 10;
