5ed5a872bfa6e92970efede36f048bf1fbfe23f8
Victor Ogorodnov <vitek984@gmail.com>
2025-08-16 06:26:25 +0500

    feat: pop up warning for no connection to lnd

---
+++ src/components/Modals/WarningModalContent.tsx
@@ -0,0 +1,122 @@
+import React, {useEffect, useContext} from 'react';
+import {View, StyleSheet} from 'react-native';
+import Animated from 'react-native-reanimated';
+
+import PlasmaModal from './PlasmaModal';
+import BlueButton from '../Buttons/BlueButton';
+
+import TranslateText from '../../components/TranslateText';
+import {ScreenSizeContext} from '../../context/screenSize';
+import {PopUpContext} from '../../context/popUpContext';
+
+interface Props {
+  isVisible: boolean;
+  close: () => void;
+  text?: string;
+  textKey?: string;
+  textDomain?: string;
+  disableBlur?: boolean;
+}
+
+const WarningModalContent: React.FC<Props> = props => {
+  const {isVisible, close, text, textKey, textDomain, disableBlur} = props;
+
+  const {width: SCREEN_WIDTH, height: SCREEN_HEIGHT} =
+    useContext(ScreenSizeContext);
+  const styles = getStyles(SCREEN_WIDTH, SCREEN_HEIGHT);
+
+  const {showPopUp} = useContext(PopUpContext);
+
+  const modal = (
+    <PlasmaModal
+      isOpened={isVisible}
+      close={() => close()}
+      isFromBottomToTop={true}
+      animDuration={250}
+      gapInPixels={0}
+      backSpecifiedStyle={{backgroundColor: 'transparent'}}
+      disableBlur={disableBlur}
+      renderBody={(_, __, ___, ____, cardTranslateAnim) => (
+        <Animated.View style={[styles.modal, cardTranslateAnim]}>
+          <View style={styles.textContainer}>
+            {text ? (
+              <TranslateText
+                textValue={text}
+                maxSizeInPixels={SCREEN_HEIGHT * 0.024}
+                textStyle={styles.text}
+                numberOfLines={5}
+              />
+            ) : textKey && textDomain ? (
+              <TranslateText
+                textKey={textKey}
+                domain={textDomain}
+                maxSizeInPixels={SCREEN_HEIGHT * 0.024}
+                textStyle={styles.text}
+                numberOfLines={5}
+              />
+            ) : (
+              <></>
+            )}
+          </View>
+          <View style={styles.button}>
+            <BlueButton
+              textKey="got_it"
+              textDomain="main"
+              onPress={() => close()}
+            />
+          </View>
+        </Animated.View>
+      )}
+    />
+  );
+
+  useEffect(() => {
+    showPopUp(modal);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [isVisible, close, text, disableBlur]);
+
+  return <></>;
+};
+
+const getStyles = (screenWidth: number, screenHeight: number) =>
+  StyleSheet.create({
+    modal: {
+      position: 'absolute',
+      bottom: screenHeight * 0.35,
+      left: screenWidth * 0.1,
+      backgroundColor: '#fff',
+      width: screenWidth * 0.8,
+      height: screenHeight * 0.3,
+      alignItems: 'center',
+      borderTopLeftRadius: 30,
+      borderTopRightRadius: 30,
+      borderBottomLeftRadius: 30,
+      borderBottomRightRadius: 30,
+      shadowColor: '#000',
+      shadowOffset: {
+        width: 0,
+        height: 1,
+      },
+      shadowOpacity: 0.07,
+      shadowRadius: 3,
+    },
+    textContainer: {
+      flex: 1,
+      justifyContent: 'center',
+      alignItems: 'center',
+      padding: screenHeight * 0.025,
+    },
+    text: {
+      color: '#000',
+      fontSize: screenHeight * 0.02,
+      fontWeight: 'bold',
+      textAlign: 'center',
+      letterSpacing: -0.18,
+    },
+    button: {
+      width: screenWidth * 0.8 - screenHeight * 0.04,
+      marginBottom: screenHeight * 0.02,
+    },
+  });
+
+export default WarningModalContent;
